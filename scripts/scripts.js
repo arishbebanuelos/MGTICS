d079b5326c40a4ec5fc64def50c9a0a5d5ec17b3
arishbebanuelos <arishbe.banuelos@uabc.edu.mx>
2025-02-26 20:53:04 -0800

    MGTICS

---
+++ scripts/scripts.js
@@ -0,0 +1,33 @@
+// keyword variablename = value
+//val, let, const
+
+//string 
+var firstName = "Arishbe";
+var lastName = "Dudlley";
+
+//concatenacion
+console.log("Name: "+ firstName + " Last name: "+ lastName);
+
+
+firstName = "Daniel";
+console.log(firstName,lastName);
+
+//numerico
+let myAge=26;
+let myHeight= 155;
+
+console.log(myAge,myHeight);
+
+//bolean
+let isStudent = true;
+
+//math operations
+let num1=10;
+let num2=12;
+
+document.write("El resultado es: " + (num1+num2)+ "</p>");
+document.write("El resultado es: " + (num1-num2));
+document.write("El resultado es: " + (num1*num2));
+document.write("El resultado es: " + (num1/num2).toFixed(3));
+
+alert("Este es un mensaje de ALERTA");
